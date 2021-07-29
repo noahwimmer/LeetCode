10672a5ced525fb86ee30f0f0453674929eac0f9
noahwimmer <noah.wimmer10@gmail.com>
2021-07-29 11:18:35 -0500

    refactored TwoSumSolution.java

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,28 @@
+import java.util.Random;
+
 public class Main {
 
     public Main() {
         // test solutions here
-        ValidPalandromeSolution sol = new ValidPalandromeSolution();
-        String testStr = "Marge, let's \"[went].\" I await {news} telegram.";
-        System.out.println(sol.isPalindrome(testStr));
+        TwoSumSolution sumSolution = new TwoSumSolution();
+
+        Random r = new Random();
+
+        int[] arr = new int[10000];
+        // fill arr with dummy data
+        for(int i = 0; i < arr.length; i++) {
+            arr[i] = r.nextInt(100);
+        }
+
+        int target = 158;
+        int[] result = sumSolution.twoSum(arr, target);
+        System.out.println("{" + result[0] + ", " + result[1] + "}");
     }
 
     public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
         new Main();
+        long endTime = System.currentTimeMillis();
+        System.out.println("That took: " + (endTime - startTime) + " ms");
     }
 }
--- src/TwoSumSolution.java
+++ src/TwoSumSolution.java
@@ -4,30 +4,24 @@ Given an array of integers nums and an integer target, return indices of the two
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
 You can return the answer in any order.
- */
-//Verified
+*/
+
+import java.util.HashMap;
+
 public class TwoSumSolution {
     public int[] twoSum(int[] nums, int target) {
-        if(nums.length < 2) {
-            int[] solution = {-1, -1};
-            return solution;
-        }
-        if(nums.length == 2 && nums[0] + nums[1] == target) {
-            int[] solution = {0, 1};
-            return solution;
-        } else if(nums.length == 2){
-            int[] solution = {-1, -1};
-            return solution;
-        }
-        for(int i = 0; i < nums.length; i++) {
-            for(int j = i + 1; j <= nums.length - 1; j++) {
-                if(nums[i] + nums[j] == target) {
-                    int[] solution = {i, j};
-                    return solution;
-                }
+        // One pass hash map
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
             }
+            map.put(nums[i], i);
         }
-        int[] noSol = {-1, -1};
-        return noSol;
+
+        return null;
     }
 }
